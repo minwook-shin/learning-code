c25fc3d9aafb00e73fd1a50992a6317515ff8e51
minwook <minwook0106@gmail.com>
2017-11-18 22:42:56 +0900

    upload var

---
+++ variables/variables/variables.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a = 10;
+
+	bool b = true;
+
+	char c = 'c';
+
+	float d = 3.14f;
+
+	double e = 123.123;
+
+	cout << a << endl;
+	cout << b << endl;
+	cout << c << endl;
+	cout << d << endl;
+	cout << e << endl;
+	return 0;
+}
