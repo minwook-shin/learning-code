f530fb2a0c83721f0d000fbfa0912c85af733075
minwook <minwook0106@gmail.com>
2018-01-24 14:36:17 +0900

    inti VER commit

---
+++ cpp/ubuntu_cpp_study_final/functions.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<fstream>
+#include<map>
+
+using namespace std;
+
+class management
+{
+public:
+	management();
+	~management();
+
+	string name;
+	int score;
+	int select_num = 0;
+	vector<string>user_info = {};
+	vector<int>user_score = {};
+	map<string, int>sort_info = {};
+	map<string, int>::iterator j;
+	string find_user;
+	string del_user;
+	int edit_score;
+	string str_score;
+
+	string print_all_info(int score, string name, string grade);
+	void enter_info();
+	void search_user();
+	void edit_user();
+	void delete_user();
+	void print_user();
+	void sort_user();
+	int close_program();
+	int wellcome();;
+private:
+
+};
+
+
+
+
+
---
+++ cpp/ubuntu_cpp_study_final/main.cpp
@@ -0,0 +1,37 @@
+#include"functions.h"
+
+int main() 
+{
+	management main;
+	
+	while (true)
+	{
+		main.select_num = main.wellcome();
+		switch (main.select_num)
+		{
+		case 1:
+			main.enter_info();
+			break;
+		case 2:
+			main.search_user();
+			break;
+		case 3:
+			main.edit_user();
+			break;
+		case 4:
+			main.delete_user();
+			break;
+		case 5:
+			main.print_user();
+			break;
+		case 6:
+			main.sort_user();
+			break;
+		case 7:
+			main.close_program();
+		default:
+			exit(0);
+		}
+	}
+	return 0;
+}
