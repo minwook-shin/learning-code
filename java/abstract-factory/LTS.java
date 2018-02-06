eec0cf98835a7347d63399ff836cdceef893b4bf
minwook <minwook0106@gmail.com>
2018-02-06 19:51:27 +0900

    learn A.F pattern

---
+++ java/abstract-factory/LTS.java
@@ -0,0 +1,10 @@
+package src;
+
+public class LTS implements Ubuntu {
+
+	@Override
+	public void bash() {
+		System.out.println("long term service");
+		
+	}
+}
---
+++ java/abstract-factory/Ubuntu.java
@@ -0,0 +1,5 @@
+package src;
+
+public interface Ubuntu {
+	public void bash();
+}
