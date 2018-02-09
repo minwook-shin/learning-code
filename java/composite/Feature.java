e50110301f7597b8c8f786e3c10a05eddaa3eb96
minwook <minwook0106@gmail.com>
2018-02-09 18:24:56 +0900

    learn composite

---
+++ java/composite/Feature.java
@@ -0,0 +1,5 @@
+package composite;
+
+public interface Feature {
+	public void print();
+}
---
+++ java/composite/File.java
@@ -0,0 +1,10 @@
+package composite;
+
+public class File implements Feature{
+
+	@Override
+	public void print() {
+		System.out.println("print");
+	}
+
+}
