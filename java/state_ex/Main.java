91851a5c1d8a1e9a45980c99c86d055dd34cfa92
minwook <minwook0106@gmail.com>
2018-02-16 17:43:01 +0900

    learn state

---
+++ java/state_ex/Main.java
@@ -0,0 +1,16 @@
+package state_ex;
+
+public class Main {
+
+	public static void main(String[] args) {
+		state test = new state();
+		
+		test.off();
+		test.on();
+		test.on();
+		test.off();
+		test.off();
+
+	}
+
+}
