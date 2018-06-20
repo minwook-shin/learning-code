e3a0c6a70eee7e838fe50f712889c54a250e1869
minwook-shin <minwook0106@gmail.com>
2018-06-20 14:39:27 +0900

    add new app

---
+++ android/navbar/app/src/main/java/com/example/minwook/navbar/first.java
@@ -0,0 +1,18 @@
+package com.example.minwook.navbar;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class first extends Fragment {
+    View v;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        v = inflater.inflate(R.layout.first, container, false);
+        return v;
+    }
+}
---
+++ android/navbar/app/src/main/java/com/example/minwook/navbar/second.java
@@ -0,0 +1,18 @@
+package com.example.minwook.navbar;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class second extends Fragment {
+    View v;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        v = inflater.inflate(R.layout.second, container, false);
+        return v;
+    }
+}
