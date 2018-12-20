1db2707dcafcb1472404f6881cb0f30cf1bdb114
minwook-shin <minwook0106@gmail.com>
2018-12-20 21:15:08 +0900

    learn Navigation Basics
    Signed-off-by: minwook-shin <minwook0106@gmail.com>

---
+++ flutter/navigate_new_screen/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Navigation Basics',
+    home: FirstScreen(),
+  ));
+}
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('메인 화면'),
+      ),
+      body: Center(
+        child: OutlineButton(
+          child: Text('다음 페이지'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondScreen()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("열린 페이지"),
+      ),
+      body: Center(
+        child: OutlineButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('뒤로가기'),
+        ),
+      ),
+    );
+  }
+}
