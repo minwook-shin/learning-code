2264e5ee0c7c573acb82d6b3c268f1c0c6a0e124
minwook-shin <minwook0106@gmail.com>
2019-02-18 05:03:34 +0900

    learn vue cli add component
    Signed-off-by: minwook-shin <minwook0106@gmail.com>

---
+++ vue/add_component_test/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <NewLocalWorld></NewLocalWorld>
+    <NewGlobalWorld></NewGlobalWorld>
+  </div>
+</template>
+
+<script>
+import NewLocalWorld from './components/NewLocalWorld';
+
+export default {
+  name: 'app',
+  components: {
+    NewLocalWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
