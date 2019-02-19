ac401dbe840140f8b23a47ee232ee3c734a3b436
minwook-shin <minwook0106@gmail.com>
2019-02-19 16:48:28 +0900

    learn todo app
    Signed-off-by: minwook-shin <minwook0106@gmail.com>

---
+++ vue/todo-app/src/components/TodoHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <h1>ToDo App</h1>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+h1 {
+  color: deepskyblue;
+  font-weight: 1000px;
+  margin: 3rem 0 1rem 0;
+}
+</style>
---
+++ vue/todo-app/src/components/TodoList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <ul>
+      <li v-bind:key="item" v-for="(item,index) in propsData">
+        {{item}}
+        <button v-on:click="removeItem(item, index)">
+          <i class="fas fa-trash"></i>
+        </button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    removeItem: function(item, index) {
+      this.$emit("RemoveTodoItem", item, index);
+    }
+  },
+  props: ["propsData"]
+};
+</script>
+
+<style>
+</style>
