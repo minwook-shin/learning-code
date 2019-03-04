fbaab02ee9f491c144adc6696c63b8315a226422
minwook-shin <minwook0106@gmail.com>
2019-03-05 05:54:20 +0900

    learn es6 spread and vuex helper function
    Signed-off-by: minwook-shin <minwook0106@gmail.com>

---
+++ vue/vuex-test-helper-func/src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello">
+    <div>{{text}}</div>
+    <div>{{num}}</div>
+    <button v-on:click="pushedButton(10)">up</button>
+    <button v-on:click="pushedButton2(10)">up</button>
+  </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+import { mapGetters } from "vuex";
+import { mapMutations } from "vuex";
+import { mapState } from "vuex";
+
+export default {
+  methods: {
+    ...mapActions(["addNum"]),
+    pushedButton(num) {
+      this.addNum({
+        num: num
+      });
+    },
+    ...mapMutations({ add: "addNum" }),
+    pushedButton2(num) {
+      this.add({
+        value: num
+      });
+    }
+  },
+  computed: {
+    ...mapGetters(["text"]),
+    ...mapState(["num"])
+  }
+};
+</script>
+
+<style scoped>
+</style>
