deadb6bd463ae8350d3d2e6f1ec9e17d00c310e0
minwook-shin <minwook0106@gmail.com>
2019-03-21 05:52:50 +0900

    learn webview_flutter
    Signed-off-by: minwook-shin <minwook0106@gmail.com>

---
+++ flutter/flutter_webview_test/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter webview',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: WebView(
+        initialUrl: 'https://flutter.dev',
+        javascriptMode: JavascriptMode.unrestricted,
+        onPageFinished: (String url) {
+          print('finished:' + url);
+        },
+      ),
+    );
+  }
+}
