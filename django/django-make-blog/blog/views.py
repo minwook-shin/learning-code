19e5547ce600875ca23c98ef8ac8e3d05fc3c422
minwook-shin <minwook0106@gmail.com>
2019-03-24 18:59:22 +0900

    add django template
    Signed-off-by: minwook-shin <minwook0106@gmail.com>

--- django/django-make-blog/blog/views.py
+++ django/django-make-blog/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+
+def postList(request):
+    posts = Post.objects.all()
+    return render(request, 'post_list.html', {'posts': posts})
