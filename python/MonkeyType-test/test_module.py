ee45b3c9a5f4f9d2411228b648d2be6d372b1604
minwook-shin <minwook0106@gmail.com>
2019-04-03 08:40:50 +0900

    learn monkeytype
    Signed-off-by: minwook-shin <minwook0106@gmail.com>

---
+++ python/MonkeyType-test/test_module.py
@@ -0,0 +1,14 @@
+def add(a: int, b: int) -> int:
+    return a + b
+
+
+def sub(a: int, b: int) -> int:
+    return a - b
+
+
+def mul(a: int, b: int) -> int:
+    return a * b
+
+
+def div(a: int, b: int) -> float:
+    return a / b
