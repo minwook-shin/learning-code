d020a940125439051a63109f48665e954860d7eb
minwook-shin <minwook0106@gmail.com>
2019-04-26 06:14:39 +0900

    learn python Matplotlib
    Signed-off-by: minwook-shin <minwook0106@gmail.com>

---
+++ python/matplotlib-test/line.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+plt.plot([1, 2, 3, 4])
+
+plt.xlabel('x label')
+plt.ylabel('y label')
+
+plt.title("Plot")
+
+plt.legend(['example'])
+
+plt.show()
---
+++ python/matplotlib-test/no-figure.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+
+fig = plt.figure()
+fig.suptitle('No figure')
+
+fig, ax_lst = plt.subplots(2, 2)
+
+plt.show()
