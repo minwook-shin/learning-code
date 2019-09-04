89a1b9bd36e128014bfa828263105f4d8c00ed08
minwook-shin <minwook0106@gmail.com>
2019-09-05 06:06:51 +0900

    learn go jennifer

--- go/jennifer_test/main.go
+++ go/jennifer_test/main.go
@@ -7,9 +7,9 @@ import (
 )
 
 func main() {
-	f := jen.NewFile("main")
-	f.Func().Id("main").Params().Block(
+	file := jen.NewFile("main")
+	file.Func().Id("main").Params().Block(
 		jen.Qual("fmt", "Println").Call(jen.Lit("Hello, world")),
 	)
-	fmt.Printf("%#v", f)
+	fmt.Printf("%#v", file)
 }
