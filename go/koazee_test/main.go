dfed10553bca4ec930915774406dc6c8cadf3fd1
minwook-shin <minwook0106@gmail.com>
2019-11-15 05:59:49 +0900

    learn go koazee

--- go/koazee_test/main.go
+++ go/koazee_test/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	intArray := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	intArray := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	fmt.Println(intArray)
 
 	stream := koazee.StreamOf(intArray)
